024503c280afde37a88de67b7d7ee15559687d16
Mack <mldixon9750@gmail.com>
2024-02-12 22:13:26 -0800

    added logic to database.js to get all content from db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -10,7 +10,8 @@ const initdb = async () =>
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
-  });
+  }
+);
 
 // (Add logic to a method that accepts some content + adds it to the database)
 export const putDb = async (content)  => {
@@ -33,4 +34,17 @@ export const putDb = async (content)  => {
   console.log('💾 - data saved to the database', result);
 };
 
+// (Add logic for a method that gets all the content from the database)
+export const getDb = async () => {
+  console.log('Get from the database');
+  const contactDb = await openDB('jate', 1);
+  const tx = contactDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  // Use the .getAll() method to get all data in the database
+  const request = store.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
+  
 initdb();
